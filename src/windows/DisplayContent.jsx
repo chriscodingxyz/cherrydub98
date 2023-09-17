45dc61d1bf387340ba17006b34891b23ce9a4202
cherrydub <cherrydub@gmail.com>
2023-09-17 18:44:43 +0100

    tron and nyan gif backgrounds added

--- src/windows/DisplayContent.jsx
+++ src/windows/DisplayContent.jsx
@@ -41,14 +41,19 @@ export default function DisplayContent({
             {/* <option value="background-pepe3">Pepe 2</option> */}
             <option value="background-ashpika">Pokemon</option>
             <option value="background-dbz1">DragonBall Z</option>
-            <option value="background-robocop">Robocop</option>
+            <option value="background-tron">Tron</option>
+            <option value="background-nyan">Nyan</option>
+            {/* <option value="background-robocop">Robocop</option> */}
             <option value="background-default">Classic: Green</option>
           </>
         ) : (
           <>
             <option value="background-pepe1">Pepe 1</option>
+            <option value="background-tron">Tron</option>
+            <option value="background-nyan">Nyan</option>
             {/* <option value="background-pepe3">Pepe 2</option> */}
             <option value="background-default">Classic: Green</option>
+
             {/* <option value="background-98-sky-logo">98-Sky</option> */}
             {/* <option value="background-98-cougar">98-Cougar</option> */}
             {/* <option value="background-98-leopard">98-Leopard</option>
