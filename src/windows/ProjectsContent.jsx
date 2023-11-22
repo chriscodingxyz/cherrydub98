11e31aff19534596f477bb734d1b3b6b903bce1e
cherrydub <cherrydub@gmail.com>
2023-11-22 18:55:41 +0000

    links updated

--- src/windows/ProjectsContent.jsx
+++ src/windows/ProjectsContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ProjectsContent() {
   return (
     <div className="flex flex-wrap p-4 gap-4">
-      <a target="_blank" href="https://cherrydub.com/">
+      <a target="_blank" href="https://portfolio.cherrydub.com/">
         <div className="flex flex-col items-center overflow-auto">
           <img
             src="https://win98icons.alexmeub.com/icons/png/html-5.png"
