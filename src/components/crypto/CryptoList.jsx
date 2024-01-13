e8c34cea491fb2fefe72022f7d98ecae640cf85e
cherrydub <cherrydub@gmail.com>
2024-01-13 00:18:03 +0000

    refactored cryptolist to be tables

--- src/components/crypto/CryptoList.jsx
+++ src/components/crypto/CryptoList.jsx
@@ -22,23 +22,9 @@ const currencyObj = {
   gbp: "£",
 };
 
-export default function CryptoList(
-  {
-    // coins,
-    // currency,
-    // setCurrency,
-    // setDisplayAmount,
-  }
-) {
+export default function CryptoList() {
   const [displayAmount, setDisplayAmount] = useState(25);
   const [currency, setCurrency] = useState("usd");
-
-  const currencySymbol = {
-    usd: "$",
-    gbp: "£",
-    eur: "€",
-  };
-
   const [coins, setCoins] = useState([]);
 
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${displayAmount}&page=1&sparkline=false&locale=en`;
@@ -66,11 +52,12 @@ export default function CryptoList(
   function sortOnChange(e) {
     setOrderSort(e.target.value);
   }
+
   return (
     <>
       <div className="flex">
         <div className="flex-1 text-center">
-          currency{" "}
+          Currency{" "}
           <select name="" id="" onChange={currencyOnChange} value={currency}>
             <option value="usd">$</option>
             <option value="gbp">£</option>
@@ -78,7 +65,7 @@ export default function CryptoList(
           </select>
         </div>
         <div className="flex-1">
-          display amount
+          Display Amount
           <select name="" id="" onChange={displayOnChange}>
             <option value="25">25</option>
             <option value="100">100</option>
@@ -86,44 +73,45 @@ export default function CryptoList(
           </select>
         </div>
       </div>
-      <div className="flex flex-col">
-        <div className="flex justify-between bg-gray-200 p-1">
-          <div className="flex items-center space-x-2 flex-1">
-            <div className="flex-1 font-bold"># Coin</div>
-          </div>
-          <div className="flex-1 text-right font-bold">Price</div>
-          <div className="flex-1 text-right font-bold">Market Cap</div>
-        </div>
 
-        {coins.map((coin, index) => (
-          <div
-            key={coin.id}
-            className="flex items-center justify-between rounded-md shadow-md p-1 border"
-          >
-            <div className="flex items-center space-x-2  flex-1">
-              <div className="text-gray-600">{index + 1}</div>
-              <img
-                src={coin.image}
-                alt={coin.name}
-                className="w-4 h-4 rounded-full"
-              />
-              <span className="text-md font-bold">{coin.symbol}</span>
-            </div>
-
-            <div className="flex-1 text-md text-right">
-              {coin.current_price.toLocaleString("en-US", {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2,
-              })}
-              <span className="font-bold">{" " + currencyObj[currency]}</span>
-            </div>
-
-            <div className="flex-1 text-md text-right ">
-              {abbreviateNumber(coin.market_cap)}
-            </div>
-          </div>
-        ))}
-      </div>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th className="col-span-2">Coin</th>{" "}
+            {/* Use col-span-2 to span two columns */}
+            <th>Price</th>
+            <th>Market Cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin, index) => (
+            <tr key={coin.id}>
+              <td className="border">{index + 1}</td>
+              <td className="border col-span-2">
+                <div className="flex items-center space-x-2">
+                  <img
+                    src={coin.image}
+                    alt={coin.name}
+                    className="w-4 h-4 rounded-full "
+                  />
+                  <span className="text-md font-bold">{coin.symbol}</span>
+                </div>
+              </td>
+              <td className="border text-right">
+                {coin.current_price.toLocaleString("en-US", {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+                <span className="font-bold">{" " + currencyObj[currency]}</span>
+              </td>
+              <td className="border text-right">
+                {abbreviateNumber(coin.market_cap)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
